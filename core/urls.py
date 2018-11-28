ffbcaefbb662e9b5262537f7e9fedacbe05f65fb
hector <hec.jara@alumnos.duoc.cl>
2018-11-28 16:45:10 -0300

    agregado ventana administrar listass, funcionalidad eliminar lista

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from .views import home
+from .views import home, listar_listas, eliminar_lista
 
 
 
 
 urlpatterns = [
     path('', home, name="home"),
+    path('listar-listas/', listar_listas, name="listado_listas"),
+    path('eliminar-lista/<id>/', eliminar_lista, name="eliminar_lista"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import EstadoLista, EstadoProducto, EstadoTienda, Producto, Lista, Region, Comuna, Tienda
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+#CRUD DE LISTAS
+
+def listar_listas(request):
+
+    listas = Lista.objects.all()
+
+    return render(request, 'core/listar_listas.html', {
+        'listas':listas
+    })
+
+
+def eliminar_lista(request, id):
+    #buscar la lista a eliminar
+    lista = Lista.objects.get(id=id)
+
+    try:
+        lista.delete()
+        mensaje = "Eliminado correctamente"
+        messages.success(request, mensaje)
+    except:
+        mensaje = "Error al intentar eliminar"
+        messages.error(request, mensaje)
+
+    return redirect('listado_listas')
