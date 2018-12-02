f07dd5df6dd866f6a583ab1b8bc7827278668120
hector <hec.jara@alumnos.duoc.cl>
2018-12-02 18:35:05 -0300

    se agrega el directorio accounts para empezar a trabajar con el login y registro de cuentas

--- examen_dwy/settings.py
+++ examen_dwy/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'accounts',
     'api',
     'pwa',
     'fcm_django',
--- examen_dwy/urls.py
+++ examen_dwy/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('djangi.contrib.auth.urls')),
     path('api/', include('api.urls')),
     path('', include('pwa.urls')),
 ]
