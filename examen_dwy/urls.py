40c0fe45c6635b1a115936580596215f04ac8a39
hector <hec.jara@alumnos.duoc.cl>
2018-12-03 17:01:07 -0300

    se arregla error tipografico en el path de accounts

--- examen_dwy/urls.py
+++ examen_dwy/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('djangi.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
     path('', include('pwa.urls')),
 ]
