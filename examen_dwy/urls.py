5cfae3193df5d3a6cd13687d83520446a75647d5
hector <hec.jara@alumnos.duoc.cl>
2018-12-12 15:33:37 -0300

    se agrega el registro de usuario (ailyn)

--- examen_dwy/urls.py
+++ examen_dwy/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('api/', include('api.urls')),
     path('', include('pwa.urls')),
 ]
